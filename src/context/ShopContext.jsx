79882ad1aca161630ea1d35790584c95e209eb0a
kangetheee <2003kangethe@gmail.com>
2024-10-11 17:54:52 +0300

    final-i-think

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState(""); 
     const [all_products, setAll_products] = useState([]);
-    const url = "http://localhost:4000";
+    const url = "https://delight-api-odzw.onrender.com";
 
     // Add to Cart
     const addToCart = async (itemId) => {
